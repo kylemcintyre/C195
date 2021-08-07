1567504de3f334452c40efc1cf6163f709cc145e
Kyle McIntyre <kyletmcintyre@gmail.com>
2021-08-07 00:38:16 -0400

    added all controllers and fxml

---
+++ C195/src/Views/AddAppointmentController.java
@@ -0,0 +1,29 @@
+package Views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AddAppointmentController {
+
+    @FXML
+    private Label addAppointmentLabel;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void cancelButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonClicked(ActionEvent event) {
+
+    }
+
+}
--- C195/src/Views/MainScreenController.java
+++ C195/src/Views/MainScreenController.java
@@ -5,12 +5,36 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class MainScreenController {
+
+    @FXML
+    private TableView<?> customerTable;
+
+    @FXML
+    private TableColumn<?, ?> customerIDCol;
+
+    @FXML
+    private TableColumn<?, ?> customerNameCol;
+
+    @FXML
+    private TableColumn<?, ?> customerAddressCol;
+
+    @FXML
+    private TableColumn<?, ?> customerPostalCol;
+
+    @FXML
+    private TableColumn<?, ?> customerCountryCol;
+
+    @FXML
+    private TableColumn<?, ?> customerPhoneCol;
+
+    @FXML
+    private TableColumn<?, ?> customerDivisionCol;
 
     @FXML
     private TableView<?> appointmentTable;
@@ -19,21 +43,188 @@ public class MainScreenController {
     private TableColumn<?, ?> appointmentIDCol;
 
     @FXML
-    private TableColumn<?, ?> userIDCol;
+    private TableColumn<?, ?> appointmentTitleCol;
 
     @FXML
-    private TableColumn<?, ?> customerIDCol;
+    private TableColumn<?, ?> appointmentDescriptionCol;
 
     @FXML
-    private TableColumn<?, ?> titleCol;
+    private TableColumn<?, ?> appointmentLocationCol;
 
     @FXML
-    private TableColumn<?, ?> startCol;
+    private TableColumn<?, ?> appointmentContactCol;
 
     @FXML
-    private TableColumn<?, ?> endCol;
+    private TableColumn<?, ?> appointmentTypeCol;
 
-    public MainScreenController() {
+    @FXML
+    private TableColumn<?, ?> appointmentStartCol;
+
+    @FXML
+    private TableColumn<?, ?> appointmentEndCol;
+
+    @FXML
+    private TableColumn<?, ?> appointmentCustomerIDCol;
+
+    @FXML
+    private Button addCustomerButton;
+
+    @FXML
+    private Button updateCustomerButton;
+
+    @FXML
+    private Button addAppointmentButton;
+
+    @FXML
+    private Button deleteAppointmentButton;
+
+    @FXML
+    private Button updateAppointmentButton;
+
+    @FXML
+    private Button deleteCustomerButton;
+
+    @FXML
+    private Label customersLabel;
+
+    @FXML
+    private Label appointmentsLabel;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private RadioButton allAppointmentsRadio;
+
+    @FXML
+    private ToggleGroup appointmentGroup;
+
+    @FXML
+    private RadioButton monthlyAppointmentsRadio;
+
+    @FXML
+    private RadioButton weeklyAppointmentsRadio;
+
+    @FXML
+    void addAppointmentButtonClicked(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddAppointment.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
-}
 
+    @FXML
+    void addCustomerButtonClicked(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddCustomer.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void appointmentRadioSelection(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteAppointmentButtonClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Appointment");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you wish to delete this appointment?");
+        ButtonType buttonTypeOne = new ButtonType("Yes");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);
+        alert.getDialogPane().lookupButton(buttonTypeCancel).setVisible(true);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == buttonTypeOne) {
+
+        }
+    }
+
+    @FXML
+    void deleteCustomerButtonClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Customer");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you wish to delete this customer?");
+        ButtonType buttonTypeOne = new ButtonType("Yes");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);
+        alert.getDialogPane().lookupButton(buttonTypeCancel).setVisible(true);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == buttonTypeOne) {
+
+        }
+    }
+
+    @FXML
+    void logoutButtonClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you wish to logout?");
+        ButtonType buttonTypeOne = new ButtonType("Yes");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);
+        alert.getDialogPane().lookupButton(buttonTypeCancel).setVisible(true);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == buttonTypeOne) {
+            Stage stage = (Stage) logoutButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    @FXML
+    void updateAppointmentButtonClicked(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UpdateAppointment.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void updateCustomerButtonClicked(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UpdateCustomer.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void reportsButtonClicked(ActionEvent evenet) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Reports.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
